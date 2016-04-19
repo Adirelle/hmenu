bb286c47d3a95371d8d86bb91ac20acc2a6af9d8
Adirelle <adirelle@gmail.com>
2016-04-19 18:24:35 +0200

    Properly handle directory masking.

--- src/HMenu/ScanDirs.hs
+++ src/HMenu/ScanDirs.hs
@@ -4,19 +4,31 @@ module HMenu.ScanDirs (
     scanDirs
 ) where
 
-import           Control.Monad    (filterM, liftM2, mapM)
-import           HMenu.Types      (Entry)
-import           System.Directory (doesDirectoryExist, doesFileExist, listDirectory)
-import           System.FilePath  ((</>))
+import           Control.Monad
+import           Control.Monad.State
+import qualified Data.Map.Strict     as M
+import           HMenu.Types
+import           System.Directory
+import           System.FilePath
 
 scanDirs :: (FilePath -> IO Bool) -> (FilePath -> IO [Entry]) -> [FilePath] -> IO [Entry]
 scanDirs fileFilter converter dirs = do
-    existingDirs <- filterM doesDirectoryExist dirs
-    files <- concat <$> mapM listDirFiles existingDirs
-    concat <$> mapM converter files
+    files <- listFiles fileFilter dirs
+    entries <- mapM converter files
+    return $ concat entries
+
+type FileMap = M.Map FilePath FilePath
+
+listFiles :: (FilePath -> IO Bool) -> [FilePath] -> IO [FilePath]
+listFiles fileFilter dirs = do
+    files <- execStateT go M.empty
+    return $ M.elems files
     where
-        listDirFiles path = do
-            names <- map (path </>) <$> listDirectory path
-            filterM keepFile names
-        keepFile path = doesFileExist path <&&> fileFilter path
-        (<&&>) = liftM2 (&&)
+        go = forM_ dirs $ \dir -> do
+            files <- liftIO $ getDirectoryContents dir
+            forM_ files $ \name -> do
+                let path = dir </> name
+                exists <- liftIO $ doesFileExist path
+                when exists $ do
+                    valid  <- liftIO $ fileFilter path
+                    when valid $ modify' $ M.insert name path
