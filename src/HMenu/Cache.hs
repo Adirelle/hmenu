02ea8d42536f9c037d6de6f2794d682071f40f43
Adirelle <adirelle@gmail.com>
2016-04-25 00:07:57 +0200

    Refactored caching using Control.Concurrent.Async.

--- src/HMenu/Cache.hs
+++ src/HMenu/Cache.hs
@@ -2,6 +2,7 @@
 
 module HMenu.Cache (setupCache) where
 
+import           Control.Concurrent.Async
 import           Control.DeepSeq
 import           Data.Binary
 import           Data.Hashable
@@ -9,8 +10,6 @@ import           System.Directory
 import           System.FilePath
 
 import           Xdg.Directories
-
-type Cache a = MVar (CachedData a)
 
 data CachedData a = CachedData Int a
     deriving (Eq, Show, Generic)
@@ -21,39 +20,38 @@ setupCache :: (Hashable b, Binary a, NFData a) => FilePath -> b -> (b -> a) -> I
 setupCache name input func = do
     path <- cacheFilePath name
     cache <- newEmptyMVar
-    chan <- newEmptyMVar
-    fork $ retrieve cache path chan
-    fork $ refresh cache chan path input func
-    return $ reader cache
+    setupCache' path cache input func
+    return $ readMVar cache
+
+setupCache' :: (Hashable b, Binary a, NFData a) => FilePath -> MVar a -> b -> (b -> a) -> IO ()
+setupCache' path var input func = do
+    aHash <- async calcHash
+    aCachedHash <- async retrieve
+    void $ async $ refresh aHash aCachedHash
     where
-        reader :: Cache a -> IO a
-        reader cache = do
-            CachedData h value <- readMVar cache
-            return value
+        calcHash = do
+            let value = hash input
+            value `seq` return value
+        retrieve = do
+            input <- readCache path
+            case input of
+                Just (CachedData h value) -> do
+                    putMVar var value
+                    return $ Just h
+                _ ->
+                    return Nothing
+        refresh aHash aCachedHash = do
+            (current, maybeOld) <- waitBoth aHash aCachedHash
+            case maybeOld of
+                Just old | current == old ->
+                    return ()
+                Nothing -> do
+                    _ <- tryReadMVar var
+                    let value = func input
+                    value `deepseq` putMVar var value
+                    writeCache path $ CachedData current value
 
-retrieve :: Binary a => Cache a -> FilePath -> MVar (Maybe (CachedData a)) -> IO ()
-retrieve cache path chan = do
-    maybeData <- readCache path
-    case maybeData of
-        Just v -> putMVar cache v
-        _      -> return ()
-    putMVar chan maybeData
-
-refresh :: (Hashable b, Binary a, NFData a) => Cache a -> MVar (Maybe (CachedData a)) -> FilePath -> b -> (b -> a) -> IO ()
-refresh cache chan path input func = do
-    let h = hash input
-    maybeData <- h `seq` readMVar chan
-    case maybeData of
-        Just (CachedData h' _) | h == h' ->
-            return ()
-        _ -> do
-            _ <- tryTakeMVar cache
-            let value = func input
-                freshData = value `deepseq` CachedData h value
-            putMVar cache freshData
-            writeCache path freshData
-
-writeCache :: Binary a => FilePath ->CachedData a -> IO ()
+writeCache :: Binary a => FilePath -> CachedData a -> IO ()
 writeCache path dataToCache = ensureParent path >> encodeFile path dataToCache
 
 readCache :: Binary a => FilePath -> IO (Maybe (CachedData a))
