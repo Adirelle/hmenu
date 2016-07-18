1f32e4ddf04e62a8a545f733647d05400ae283dd
Adirelle <adirelle@gmail.com>
2016-07-18 09:39:56 +0200

    Refactored the cache to only expose basic read/write functions.

--- src/HMenu/Cache.hs
+++ src/HMenu/Cache.hs
@@ -1,10 +1,8 @@
-{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
-module HMenu.Cache (setupCache) where
+module HMenu.Cache where
 
 import           ClassyPrelude
-import           Control.Concurrent.Async
 import           Control.DeepSeq
 import           Data.Binary
 import           Data.Hashable
@@ -13,71 +11,30 @@ import           System.FilePath
 
 import           XDG.Directories
 
-data CachedData a = CachedData Int a
-    deriving (Eq, Show, Generic)
+readCache :: Binary a => FilePath -> IO (Maybe a)
+readCache p = do
+    r <- try (decodeFile p :: Binary a => IO a)
+    case r of
+        Right v -> return $ Just v
+        Left e -> do
+            putStrLn $ tshow p ++ ": " ++ tshow (e :: SomeException)
+            return Nothing
 
-instance Binary a => Binary (CachedData a)
-
-setupCache :: (Hashable b, Binary a, NFData a) => FilePath -> b -> (b -> a) -> IO (IO a)
-setupCache name input func = do
-    path <- cacheFilePath name
-    cache <- newEmptyMVar
-    setupCache' path cache input func
-    return $ readMVar cache
-
-setupCache' :: (Hashable b, Binary a, NFData a) => FilePath -> MVar a -> b -> (b -> a) -> IO ()
-setupCache' path var input func = do
-    aHash <- async calcHash
-    aCachedHash <- async retrieve
-    void $ async $ refresh aHash aCachedHash
+writeCache :: Binary a => FilePath -> a -> IO ()
+writeCache p d = do
+    ensureParent p
+    encodeFile p d
     where
-        calcHash =
-            let value = hash input
-            in value `seq` return value
-        retrieve = do
-            input <- readCache path
-            case input of
-                Just (CachedData h value) -> do
-                    putMVar var value
-                    return $ Just h
-                _ ->
-                    return Nothing
-        refresh aHash aCachedHash = do
-            (current, maybeOld) <- waitBoth aHash aCachedHash
-            case maybeOld of
-                Just old | current == old ->
-                    return ()
-                Nothing ->
-                    let value = func input
-                    in value `deepseq` do
-                        _ <- tryReadMVar var
-                        putMVar var value
-                        writeCache path $ CachedData current value
-
-writeCache :: Binary a => FilePath -> CachedData a -> IO ()
-writeCache path dataToCache = ensureParent path >> encodeFile path dataToCache
-
-readCache :: Binary a => FilePath -> IO (Maybe (CachedData a))
-readCache path = do
-    exists <- doesFileExist path
-    if exists then do
-        result <- decodeFileOrFail path
-        return $ case result of
-            Right v -> Just v
-            _       -> Nothing
-    else
-        return Nothing
+        ensureParent :: FilePath -> IO ()
+        ensureParent "/" = return ()
+        ensureParent "." = return ()
+        ensureParent path = do
+            let parent = takeDirectory path
+            ensureParent parent
+            unlessM (doesDirectoryExist parent) $ createDirectory parent
 
 cacheFilePath :: FilePath -> IO FilePath
 cacheFilePath n = do
     cacheDirs <- listDirectories CacheHome
     let cacheDir = headEx cacheDirs
     return $ cacheDir </> "hmenu" </> n <.> ".bin"
-
-ensureParent :: FilePath -> IO ()
-ensureParent "/" = return ()
-ensureParent "." = return ()
-ensureParent path = do
-    let parent = takeDirectory path
-    ensureParent parent
-    unlessM (doesDirectoryExist parent) $ createDirectory parent
