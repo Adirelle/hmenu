17d58402d87fcd882da9fda5bc58f4e14fbbe821
Adirelle <adirelle@gmail.com>
2016-07-18 09:41:51 +0200

    Reworking main to use a simple thread instead of Asyncs.

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,8 @@
 module Main where
 
 import           ClassyPrelude        hiding (Index)
+import           Control.Concurrent   (forkIO)
+import           Control.DeepSeq
 import qualified Graphics.UI.Gtk      as G
 import           System.Posix.Signals
 
@@ -15,20 +17,9 @@ import           HMenu.Types
 
 main :: IO ()
 main = do
-    getIndex <- prepareIndex
-    c <- runGUI instalSignalHandlers (handler getIndex)
+    s <- initSearchEngine
+    c <- runGUI instalSignalHandlers s
     maybe (return ()) execute c
-    where
-        handler :: IO (Index Entry) -> ResultHandler -> Text -> IO ()
-        handler getIndex callback text = do
-            index <- getIndex
-            let results = if null text then [] else take 10 $ search index text
-            callback $ results
-
-prepareIndex :: IO (IO (Index Entry))
-prepareIndex = do
-    entries <- (++) <$> listDesktopEntries <*> listPathEntries
-    setupCache "index" entries createIndex
 
 instalSignalHandlers :: G.Window -> IO ()
 instalSignalHandlers main = do
@@ -36,3 +27,30 @@ instalSignalHandlers main = do
     installHandler keyboardSignal handler Nothing
     installHandler softwareTermination handler Nothing
     return ()
+
+initSearchEngine :: IO SearchHandler
+initSearchEngine = do
+    v <- newEmptyMVar
+    f <- cacheFilePath "index"
+    forkIO $ do
+        x <- xdgProvider
+        p <- pathProvider
+        let ps = metaProvider [x, p]
+            h  = toHash ps
+        c <- readCache f
+        i <- case c of
+            Just (h', i) | h' == h -> return i
+            _ -> newIndex f h ps
+        i `deepseq` putMVar v i
+    return $ handler v
+    where
+        handler :: MVar (Index Entry) -> ResultHandler -> Text -> IO ()
+        handler v h t = do
+            i <- readMVar v
+            h $ search i t
+        newIndex :: FilePath -> Hash -> EntryProvider -> IO (Index Entry)
+        newIndex f h ps = do
+            es <- toEntries ps
+            let i = createIndex es
+            writeCache f (h, i)
+            return i
